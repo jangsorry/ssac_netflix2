422bff205dc45edaddfecc449bc64e86a451b6f7
jangsorry <piccolox@naver.com>
2021-10-17 16:45:46 +0900

    회원가입 버튼 이벤트 추가

--- ssac_netflix/SignUpViewController.swift
+++ ssac_netflix/SignUpViewController.swift
@@ -30,27 +30,27 @@ class SignUpViewController: UIViewController {
     }
     
     func uiSetting() {
-        emailTextField.text = "이메일 주소 또는 전화번호"
+        emailTextField.placeholder = "이메일 주소 또는 전화번호"
         emailTextField.textColor = .white
         emailTextField.font = .systemFont(ofSize: 14)
         emailTextField.textAlignment = .center
         
-        passwordTextField.text = "비밀번호"
+        passwordTextField.placeholder = "비밀번호"
         passwordTextField.textColor = .white
         passwordTextField.font = .systemFont(ofSize: 14)
         passwordTextField.textAlignment = .center
         
-        nicknameTextField.text = "닉네임"
+        nicknameTextField.placeholder = "닉네임"
         nicknameTextField.textColor = .white
         nicknameTextField.font = .systemFont(ofSize: 14)
         nicknameTextField.textAlignment = .center
         
-        locationTextField.text = "위치"
+        locationTextField.placeholder = "위치"
         locationTextField.textColor = .white
         locationTextField.font = .systemFont(ofSize: 14)
         locationTextField.textAlignment = .center
         
-        codeTextField.text = "추천 코드 입력"
+        codeTextField.placeholder = "추천 코드 입력"
         codeTextField.textColor = .white
         codeTextField.font = .systemFont(ofSize: 14)
         codeTextField.textAlignment = .center
@@ -69,4 +69,43 @@ class SignUpViewController: UIViewController {
             SwitchButton.thumbTintColor = .white
         }
     }
+    
+    @IBAction func SignUpClicked(_ sender: UIButton) {
+        
+        guard let text = emailTextField.text, text.count > 0 else {
+            return
+        }
+           print(text)
+        
+        guard let text2 = passwordTextField.text, text.count > 0 else {
+            return
+        }
+           print(text2)
+        
+        guard let text3 = nicknameTextField.text, text.count > 0 else {
+            return
+        }
+           print(text3)
+        
+        guard let text4 = locationTextField.text, text.count > 0 else {
+            return
+        }
+           print(text4)
+        
+        guard let text5 = codeTextField.text, text.count > 0 else {
+            return
+        }
+           print(text5)
+        
+        
+   
+
+    }
+    
+    
+    
+    
+    
+    
+    
 }
